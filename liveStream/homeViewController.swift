6760c8b49efc008d77a17235e358ee5a1a6c445a
Ben McKillop <ben@redbayrally.com>
2018-09-09 17:08:57 +0100

    changes

--- liveStream/homeViewController.swift
+++ liveStream/homeViewController.swift
@@ -1,33 +1,21 @@
-//
-//  homeViewController.swift
-//  liveStream
-//
-//  Created by Ben McKillop on 03/08/2018.
-//  Copyright © 2018 Ben McKillop. All rights reserved.
-//
 import UIKit
 import AVKit
 import AVFoundation
+import MultiPeer
 
 class homeViewController: UIViewController {
     
-    
     @IBOutlet weak var webViewBG: UIWebView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        MultiPeer.instance.disconnect()
         
         let htmlPath = Bundle.main.path(forResource: "WebViewContent", ofType: "html")
         let htmlURL = URL(fileURLWithPath: htmlPath!)
         let html = try? Data(contentsOf: htmlURL)
-        
         self.webViewBG.load(html!, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: htmlURL.deletingLastPathComponent())
-        
         webViewBG.scrollView.isScrollEnabled = false
-
-        
-        // Do any additional setup after loading the view.
         
     }
 
